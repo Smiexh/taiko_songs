b2388b312e28237aeab036305f0e767d3243c338
smiexh <smiexh@outlook.com>
2024-10-20 12:40:02 +0800

    ...

--- update_songs.sh
+++ update_songs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-echo "// $(date)" > taiko_songs.js
-echo "const jsonData =" >> taiko_songs.js
-python3 get_taiko_songs.py >> taiko_songs.js
+js_file_path="dist/taiko_songs.js"
+
+echo "// $(date)" > $js_file_path
+echo "const jsonData =" >> $js_file_path
+python3 get_taiko_songs.py >> $js_file_path
